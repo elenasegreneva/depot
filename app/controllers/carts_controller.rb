204e43877afbe868631fb08c88b4d3ee3ee002d5
Elena Segreneva <elena_unusova@mail.ru>
2017-02-15 15:12:29 +0300

    11.4. Шаг Е4: предотвращение отображения пустой корзины

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -57,8 +57,7 @@ rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
     @cart.destroy if @cart.id == session[:cart_id]
     session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to store_url,
-        notice: 'Ваша корзина пуста' }
+      format.html { redirect_to store_url }
       format.json { head :no_content }
     end
   end
